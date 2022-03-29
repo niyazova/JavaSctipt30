2f2e8f454350e7820899ab258d314e72197d5d1f
Niyazova_Gulnoza <niyazovagulnoza2@gmail.com>
2022-03-29 22:25:00 +0200

    changes in js

---
+++ 04.day js/js/main.js
@@ -0,0 +1,10 @@
+const inventors = [
+    { first: 'Alert', last: 'Einstain', year: 1879, passed: 1955 },
+    { first: 'Alert', last: 'Einstain', year: 1879, passed: 1955 },
+    { first: 'Alert', last: 'Einstain', year: 1879, passed: 1955 },
+    { first: 'Alert', last: 'Einstain', year: 1879, passed: 1955 },
+    { first: 'Alert', last: 'Einstain', year: 1879, passed: 1955 },
+    { first: 'Alert', last: 'Einstain', year: 1879, passed: 1955 },
+    { first: 'Alert', last: 'Einstain', year: 1879, passed: 1955 },
+];
+
