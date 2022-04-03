edd15b0488c1329e8c487ce5788476c6e72b6b34
Niyazova_Gulnoza <niyazovagulnoza2@gmail.com>
2022-04-03 15:01:13 +0200

    sort is completed

--- 04.day js/js/main.js
+++ 04.day js/js/main.js
@@ -27,6 +27,15 @@ console.table(fifteen);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate oldest to youngest
+    // const ordered = inventors.sort(function(a, b) {
+    //     if(a.year > b.year){
+    //         return 1;
+    //     } else {
+    //         return -1;
+    //     }
+    // });
+    const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+    console.table(ordered)
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
